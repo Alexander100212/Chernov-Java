32d01280ba25719579784bd6716d014d20313556
Alexander <ac6792237@gmail.com>
2025-01-22 16:42:52 +0300

    Add files via upload

---
+++ задание 2 (1).java
@@ -0,0 +1,34 @@
+//задание №1
+package test_1;
+
+import java.util.Scanner;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        scan.close();
+
+        if (a > b)
+        {
+            System.out.println("a > b");
+        }
+        else if (a < b)
+        {
+            System.out.println("a < b");
+        }
+        else
+        {
+            System.out.println("a = b");
+        }
+
+        System.out.println("a+b: " + (a + b));
+        System.out.println("a-b: " + (a - b));
+        System.out.println("a/b: " + (a / b));
+        System.out.println("axb: " + (a * b));
+    }
+}
